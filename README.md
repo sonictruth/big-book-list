e1516bb3d32a77b908350129b056f8bef6099e46
Alexandru Constantin <alex@vodafone-2.local>
2016-09-23 23:54:01 +0200

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# big-book-list
+
+> A Big Book List
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+   Test
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello';
+
+export default {
+  components: {
+    Hello,
+  },
+};
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+
+</style>
